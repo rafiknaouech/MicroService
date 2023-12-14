123f9bb093942ef05874624cafbb9f4e4da5ab19
rafiknaouech <91160830+rafiknaouech@users.noreply.github.com>
2023-12-15 00:12:00 +0100

    last-commit-front

---
+++ material-dashboard-angular2-master/src/app/typography/typography.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'app/shared.service';
+
+@Component({
+  selector: 'app-typography',
+  templateUrl: './typography.component.html',
+  styleUrls: ['./typography.component.css']
+})
+export class TypographyComponent implements OnInit {
+
+  Reservation= {
+    prix:'',
+    dateDebut:'',
+    dateFin:'',
+    duree:'',
+    
+
+  }
+
+  searchedReservationId: number;
+
+
+
+
+  reservations: any[] = [];
+
+  
+
+ 
+
+  getUniversitesList(): void {
+    this.shared.getReservationList().subscribe(
+      (data) => {
+        this.reservations = data;
+      },
+      (error) => {
+        console.error('Une erreur s\'est produite lors de la récupération des universités : ', error);
+      }
+    );
+  }
+ajout(){
+  this.shared.creatNewReservation(this.Reservation)
+   .subscribe(
+    res=>
+    {
+    console.log(res);
+    },
+    eur=>
+    {
+    console.log(eur);
+    }
+   )
+
+  }
+ 
+
+constructor(public shared: SharedService ){
+
+}
+
+ngOnInit(): void {
+  // Initialiser searchedUniversiteId à une valeur par défaut ou laisser vide selon vos besoins
+  this.searchedReservationId = 0; // ou null, ou toute autre valeur par défaut
+  this.getUniversitesList();
+}
+
+getReservationDetails(): void {
+  this.shared.getReservationById(this.searchedReservationId).subscribe(
+    (data) => {
+      this.Reservation = data;
+    },
+    (error) => {
+      console.error('Une erreur s\'est produite lors de la récupération des détails de l\'université : ', error);
+    }
+  );
+}
+
+
+deleteReservation(): void {
+  
+
+  if (!this.searchedReservationId) {
+    console.error('ID de l\'université non défini. Impossible de supprimer.');
+    return;
+  }
+
+  this.shared.deleteReservationById(this.searchedReservationId).subscribe(
+    () => {
+      console.log('Reservation supprimée avec succès ');
+      // Rediriger l'utilisateur vers la liste des universités ou une autre page après la suppression
+      
+    },
+    (error) => {
+      console.error('Une erreur s\'est produite lors de la suppression de l\'Reservation : ', error);
+    }
+  );
+}
+
+
+
+  updateReservation(): void {
+    // Supposons que vous ayez un objet d'université modifié dans votre composant
+    
+    this.shared.updateReservation({id : this.searchedReservationId ,...this.Reservation})
+      .subscribe(
+        (response) => {
+          console.log('Reservation mise à jour avec succès :', response);
+          // Traitez la réponse mise à jour ici si nécessaire
+        },
+        (error) => {
+          console.error('Erreur lors de la mise à jour de l\'Reservation :', error);
+        }
+      );
+  }
+}
